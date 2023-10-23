04bea0b39f6009359ec7c3d09efa347f98785c3b
LuluByte <demowessur@gmail.com>
2023-10-23 15:32:43 +0100

    Added Navbar component

---
+++ src/components/navbar.tsx
@@ -0,0 +1,10 @@
+export const NavBar: React.FC = () => {
+  return (
+    <div>
+      <p>Home</p>
+      <p>Unit Preview</p>
+      <p>Army Builder</p>
+      <p>Account Management</p>
+    </div>
+  );
+};
