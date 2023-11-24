9d9767116394f4c00cebfc3b9ecba6d27f591efd
LuluByte <demowessur@gmail.com>
2023-11-24 10:19:02 +0000

    Styled the category page

--- web/src/api/mock/category.ts
+++ web/src/api/mock/category.ts
@@ -1,25 +1,29 @@
 export interface ICategory {
   name: string;
   categoryId: string;
-  armyId?: string;
+  bannerURL: string;
 }
 
 const categories: ICategory[] = [
   {
     name: "Space Marines",
     categoryId: "e072ddf3-849b-47ce-b647-8f3bd2597994",
+    bannerURL: "/images/SpaceMarineBanner.png",
   },
   {
     name: "Armies of the Imperium",
     categoryId: "21a5f23c-7a83-47b6-b3e4-5a23dee45c1f",
+    bannerURL: "/images/ImperiumBanner.png",
   },
   {
     name: "Forces of Chaos",
     categoryId: "396ecb66-003e-497e-860b-04fda171f2b7",
+    bannerURL: "/images/ChaosBanner.png",
   },
   {
     name: "Xeno's Armies",
     categoryId: "699952b4-b50a-4204-ba7e-fdc637f18d25",
+    bannerURL: "/images/XenosBanner.png",
   },
 ];
 
---
+++ web/src/components/categorylink.tsx
@@ -0,0 +1,39 @@
+import styles from "@/components/categorylink.module.css";
+import { Open_Sans } from "next/font/google";
+import Link from "next/link";
+
+const Opensans = Open_Sans({ subsets: ["latin"] });
+
+interface ICategoryLink {
+  name: string;
+  categoryId: string;
+  imageURL: string;
+}
+
+export const CategoryLink: React.FC<ICategoryLink> = ({
+  name,
+  categoryId,
+  imageURL,
+}) => {
+  return (
+    <div
+      className={`${styles.linkContainer} ${Opensans.className}`}
+      style={{
+        backgroundImage: `linear-gradient(180deg, rgba(0, 0, 0, 0.00) 0%, rgba(0, 0, 0, 0.80) 80%), url(${imageURL})`,
+      }}
+    >
+      <div>
+        <h1 className={`${styles.categorytitle}`}>{name}</h1>
+        <Link
+          href={`categories/${categoryId}/armies`}
+          key={name}
+          className={`${styles.armyLink}`}
+        >
+          <button className={`${styles.categorybutton} ${Opensans.className}`}>
+            See all units
+          </button>
+        </Link>
+      </div>
+    </div>
+  );
+};
